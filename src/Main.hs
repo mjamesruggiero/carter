8e52b35a5e35384b0c8a0bd1f588f2848f838e58
mjamesruggiero <mjamesruggiero@gmail.com>
2015-02-04 16:53:21 -0800

    use streaming instead of all-in-memory

--- src/Main.hs
+++ src/Main.hs
@@ -1,20 +1,20 @@
 module Main where
 
 import qualified Data.ByteString.Lazy as BL
-import qualified Data.Vector as V
-import Data.Csv -- from cassava
+import qualified Data.Foldable as F
+import Data.Csv.Streaming
 
 type BaseballStats = (BL.ByteString, Int, BL.ByteString, Int)
 
 fourth :: (a, b, c, d) -> d
 fourth (_, _, _, d) = d
 
-baseballStats :: BL.ByteString -> Either String (V.Vector BaseballStats)
+baseballStats :: BL.ByteString -> Records BaseballStats
 baseballStats = decode NoHeader
 
 main :: IO ()
 main = do
     csvData <- BL.readFile "/Users/michaelruggiero/Desktop/batting.csv"
-    let summed = fmap (V.foldr summer 0) (baseballStats csvData)
+    let summed = F.foldr summer 0 (baseballStats csvData)
     putStrLn $ "Total atBats was: " ++ (show summed)
     where summer = (+) . fourth
